34f9a591c8884c851bd3b95560cda34a2f9aa052
couakie <couakie@free.fr>
2019-08-15 11:14:50 +0200

    16.55

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vinshelf';
+
+  constructor(){
+      // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyCPRhk0ZJCYbhmvSCZGKxmHTTXNUY2xf-g",
+        authDomain: "vinshelf.firebaseapp.com",
+        databaseURL: "https://vinshelf.firebaseio.com",
+        projectId: "vinshelf",
+        storageBucket: "",
+        messagingSenderId: "848966423475",
+        appId: "1:848966423475:web:a6fc3c8a5f6eda67"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+  }
 }
