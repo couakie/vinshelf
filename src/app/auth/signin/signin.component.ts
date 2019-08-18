aa1853a4b5b3338a10e261000fd0f75b05309f0b
couakie <couakie@free.fr>
2019-08-18 08:26:43 +0200

    26.19

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
   }
+  initForm() {
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+    const email = this.signinForm.get('email').value;
+    const password = this.signinForm.get('password').value;
+
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(['/vins']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
+
 
 }
---
+++ src/app/models/vin.model.ts
@@ -0,0 +1,6 @@
+export class Vin {
+    photo: string;
+    synopsis: string;
+    constructor(public title: string, public annee: string, public anneeMax: string, public region: string) {
+    }
+  }
